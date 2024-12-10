9cc5ec3ab809bb42885422afba23681f998f6188
Pradeep Kumar <pradeep.kumar@loginradius.com>
2024-12-11 01:37:22 +0530

    Added Tailwind Support

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -123,10 +123,22 @@ const config = {
             },
           },
         },
-      },
-       
+      }
     ],
-   "./src/plugins/orama-search"
+    async function myCustomCSSPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    }
+    ,
+   "./src/plugins/orama-search",
+
   ],
   themes: ["docusaurus-theme-openapi-docs"],
   themeConfig:
---
+++ documentation/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enables dark mode support using the 'class' strategy
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#ffff', // Light theme primary color
+          dark: '#36454F',  // Dark theme primary color
+        },
+        secondary: {
+          light: '##0b8bc4',
+          dark: '#fff',
+        },
+        
+      },
+    
+      borderRadius: {
+        button: '12px', // Custom border-radius for buttons
+      },
+    },
+  },
+  plugins: [],
+};
